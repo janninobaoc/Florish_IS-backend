715079ab9608fd5a744392e386a346618b1f3b4a
jan.nino <jan_nino.baoc@student.passerellesnumeriques.org>
2023-09-02 13:39:17 +0800

    configured api routes

--- routes/api.php
+++ routes/api.php
@@ -26,45 +26,54 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware('auth:sanctum')->group(function () {
 
     // Group for user management routes
-    Route::post('/registerUser', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::prefix('/users')->group(function () {
+        Route::post('/', [UserController::class, 'store'])->name('users.store');
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
+        Route::get('{id}', [UserController::class, 'show'])->name('users.show');
+        Route::delete('{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    });
+
+    // Group for category management routes
+    Route::prefix('/categories')->group(function () {
+        Route::post('/', [CategoryController::class, 'store'])->name('categories.store');
+        Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
+        Route::get('/{id}', [CategoryController::class, 'show'])->name('categories.show');
+        Route::put('/{category}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::delete('{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    });
+
+    // Group for brand management routes
+    Route::prefix('/brands')->group(function () {
+        Route::post('/', [BrandController::class, 'store'])->name('brands.store');
+        Route::get('/', [BrandController::class, 'index'])->name('brands.index');
+        Route::get('/{id}', [BrandController::class, 'show'])->name('brands.show');
+        Route::put('/{brand}', [BrandController::class, 'update'])->name('brands.update');
+        Route::delete('{id}', [BrandController::class, 'destroy'])->name('brands.destroy');
+    });
+
+    Route::prefix('/products')->group(function () {
+        Route::post('/', [ProductController::class, 'store'])->name('products.store');
+        //Route for critical-stock
+        Route::get('/critical-stock', [ProductController::class, 'getCriticalStock'])->name('products.critical_stock');
+        Route::get('/', [ProductController::class, 'index'])->name('products.index');
+        Route::get('{id}', [ProductController::class, 'show'])->name('products.show');
+        Route::put('{product}', [ProductController::class, 'update'])->name('products.update');
+        Route::delete('{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+    });
+
+    // Group for stock in routes
+    Route::prefix('/stockIns')->group(function () {
+        Route::post('/', [StockInController::class, 'store'])->name('stockIns.store');
+        Route::get('/', [StockInController::class, 'index'])->name('stockIns.index');
+    });
+
+    // Group for stock adjustment routes
+    Route::prefix('/stockIns')->group(function () {
+        Route::post('/doStockAdjustment', [StockAdjustmentController::class, 'store'])->name('stockAdjustments.store');
+        Route::get('/stockAdjustment', [StockAdjustmentController::class, 'index'])->name('stockAdjustments.index');
+    });
 
     // Logout route
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-    // Group for category management routes
-    Route::post('/addCategory', [CategoryController::class, 'store'])->name('categories.store');
-    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-    Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
-    Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-    Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
-
-    // Group for brand management routes
-    Route::post('/addBrand', [BrandController::class, 'store'])->name('brands.store');
-    Route::get('/brands', [BrandController::class, 'index'])->name('brands.index');
-    Route::get('/brands/{id}', [BrandController::class, 'show'])->name('brands.show');
-    Route::put('/brands/{brand}', [BrandController::class, 'update'])->name('brands.update');
-    Route::delete('/brands/{id}', [BrandController::class, 'destroy'])->name('brands.destroy');
-
-    // Group for product management routes
-    Route::post('/addProduct', [ProductController::class, 'store'])->name('products.store');
-    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-    Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-
-    // Group for stock in routes
-    Route::post('/doStockIn', [StockInController::class, 'store'])->name('stockIns.store');
-    Route::get('/stockIns', [StockInController::class, 'index'])->name('stockIns.index');
-    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-    Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-    Route::get('/critical-stock', [ProductController::class, 'getCriticalStock'])->name('products.critical_stock');
-
-    // Group for stock adjustment routes
-    Route::post('/doStockAdjustment', [StockAdjustmentController::class, 'store'])->name('stockAdjustments.store');
-    Route::get('/stockAdjustment', [StockAdjustmentController::class, 'index'])->name('stockAdjustments.index');
 });
